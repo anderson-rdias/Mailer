8ed6514e5c75e194662942315c1442a9d6e25224
Anderson Rodrigues Dias <79585797+anderson-rdias@users.noreply.github.com>
2023-12-26 09:21:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ $mail = new PHPMailer;
 $mail->isSMTP();                        // Setar o uso do SMTP
 $mail->Host = 'email-ssl.com.br';  	// Servidor smtp 
 $mail->SMTPAuth = true;                 // Habilita a autenticação do form
-$mail->Username = 'locaweb@simpsons0035.correio.biz';       // Conta de e-mail que realizará o envio
-$mail->Password = 'L1FCJ7lw#@#4';       // Senha da conta de e-mail
+$mail->Username = 'teste@btmachado.com.br';       // Conta de e-mail que realizará o envio
+$mail->Password = 'Locaweb@102030';       // Senha da conta de e-mail
 //$mail->SMTPSecure = 'tls';            // Habilitar uso do TLS (plesk 11.5 ou utilizando contas do Gmail)
 $mail->Port = 587;                       // Porta de conexão 
-$mail->From = 'locaweb@simpsons0035.correio.biz'; 			// e-mail From deve ser o mesmo de "username" (contadeEmail)
+$mail->From = 'teste@btmachado.com.br'; 			// e-mail From deve ser o mesmo de "username" (contadeEmail)
 $mail->FromName = 'Teste'; 				// Nome que será exibido ao receber a mensagem. 
-$mail->addAddress('locaweb@simpsons0035.correio.biz', 'Alexandre'); // Destinatário 
+$mail->addAddress('teste@btmachado.com.br', 'Alexandre'); // Destinatário 
 //$mail->addAddress('ellen@example.com');               	// Nome do destinatário
 //$mail->addReplyTo('info@example.com', 'Information');  	//Responder para 
 //$mail->addCC('cc@example.com'); // Adicionar cópia para o recebimento. 
